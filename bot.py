42a39e95ab8c204870ddf23a9ecaa1d69c50983b
dunya.stranger <82666721+eldar-ddos@users.noreply.github.com>
2026-03-15 00:15:48 +0100

    Refactor bot.py for better organization and clarity
    Refactor bot.py to improve structure and functionality.

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ safety_settings = [
 ]
 
 model = genai.GenerativeModel(
-    model_name='gemini-1.5-flash',
+    model_name='gemini-1.5-flash-latest', 
     system_instruction=instruction,
     safety_settings=safety_settings
 )
